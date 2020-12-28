1b565b77cf8ac6d5d0e1d3914e35d1197e056d7b
HyperAlly <matusstaviarsky@gmail.com>
2020-12-28 17:47:58 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+var api = 'http://explorer.ftscoin.xyz:11898';
+var donationAddress = "fts1eXwCpKb7hu5rG48PFEDCEVtVmME4jLvb78bWsLj8FSEcxPuwVcGLJMXmojWceUik5fxuqeeCyVEj6LBbsH891tMQgkpfCr";
+var blockTargetInterval = 120;
+var coinUnits = 100000000;
+var symbol = 'FTS';
+var refreshDelay = 10000;
+var networkStat = {
+    "fts": [
+    	["fts.miningpool.fun", "https://fts.miningpool.fun:8156"],
+        ["webfts.semipool.com", ""],
+        ["kryptokrona.compool.net", ""]
+    ]
+};
+var networkStat2 = {
+    "fts": [
+    	["fts.miningpool.fun", "https://fts.miningpool.fun:8156"],
+        ["webfts.semipool.com", ""],
+	    ["kryptokrona.compool.net", ""]
+    ]
+};
